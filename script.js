66c816dac22ebade0b46aec6caf6be88a63f6414
= <=>
2020-05-21 18:13:22 +0800

    add dblclick to go into fullscreen mode

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let t = setInterval(timer, 100);
 const m = document.querySelector('#time');
+m.addEventListener("dblclick", toggleFullScreen);
 function timer() {
     const date = new Date;
     let message = formate(date.getHours()) + ':' + formate(date.getMinutes()) + "<span id='second'>:" + formate(date.getSeconds()) + "</span>";
@@ -10,5 +11,29 @@ function formate(t) {
         return "0" + t;
     } else {
         return t;
+    }
+}
+function toggleFullScreen() {
+    if (!document.fullscreenElement &&    // alternative standard method
+        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
     }
 }
